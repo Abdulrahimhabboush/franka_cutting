784f4713253a0d6d4844c2b6c9a818208d8be376
Abdulrahim Habboush <abdulrah@stud.ntnu.no>
2021-04-18 18:43:04 +0200

    modified the test file and added another pose to the trajectory

--- scripts/test.py
+++ scripts/test.py
@@ -23,6 +23,10 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
 #pose_goal.position.x = 0.4
 #pose_goal.position.y = 0.1
 #pose_goal.position.z = 0.4
+move_group.set_named_target("home")
+
+plan = move_group.go(wait=True)
+
 move_group.set_named_target("pose2")
 
 plan = move_group.go(wait=True)
