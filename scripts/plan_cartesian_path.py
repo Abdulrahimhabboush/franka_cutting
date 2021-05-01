e1ba50a7bb1a6f1226f63ec6ffea9af429bf83ec
Abdulrahim Habboush <abdulrah@stud.ntnu.no>
2021-05-01 17:47:57 +0200

    added a new cutting strategy

---
+++ scripts/plan_cartesian_path.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+#rospy.wait_for_service("/zivid_camera/capture)
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('test', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group_name = "panda_arm"
+move_group = moveit_commander.MoveGroupCommander(group_name)
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+                                                   moveit_msgs.msg.DisplayTrajectory,
+                                                   queue_size=20)
+
+move_group = self.move_group
+
+waypoints = []
+
+wpose = move_group.get_current_pose().pose
+wpose.position.z -= scale * 0.1  # First move up (z)
+wpose.position.y += scale * 0.2  # and sideways (y)
+waypoints.append(copy.deepcopy(wpose))
+
+wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
+waypoints.append(copy.deepcopy(wpose))
+
+wpose.position.y -= scale * 0.1  # Third move sideways (y)
+waypoints.append(copy.deepcopy(wpose))
+
+(plan, fraction) = move_group.compute_cartesian_path(
+                                       waypoints,   # waypoints to follow
+                                       0.01,        # eef_step
+                                       0.0)         # jump_threshold
+   
+rospy.sleep(5)
+    
+moveit_commander.roscpp_shutdown()
